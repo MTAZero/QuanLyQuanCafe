da3b563e6663c4e8d509e6d60c8f04e35c1ac23f
MTAZero <MTAZero@MTAZERO>
2017-12-17 08:50:28 +0700

    save

--- GUI/ThongKe.cs
+++ GUI/ThongKe.cs
@@ -1,4 +1,5 @@
 ﻿using QuanLyQuanCafe.DAO;
+using QuanLyQuanCafe.DATA;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace QuanLyQuanCafe.GUI
 {
@@ -16,6 +18,8 @@ namespace QuanLyQuanCafe.GUI
         public ThongKe()
         {
             InitializeComponent();
+
+            dtpfromday.Value = DateTime.Now.AddMonths(-2);
         }
 
         #region Methods
@@ -24,7 +28,56 @@ namespace QuanLyQuanCafe.GUI
             DateTime fromday = dtpfromday.Value;
             DateTime today = dtptoday.Value;
 
-            dgrThongKe.DataSource = KhoHangDAO.ThongKe(fromday, today);
+            QuanLyQuanCafeDBContext db = new QuanLyQuanCafeDBContext();
+
+            var ListCTB =
+                        (
+                            from hd in db.HOADONBANs.Where(p => p.NGAY > fromday && p.NGAY < today).ToList()
+                            from ctb in db.CHITIETBANs.Where(p => p.MAHDB == hd.MAHDB).ToList()
+                            select new
+                            {
+                                MaMH = ctb.MAMH,
+                                SoLuong = ctb.SOLUONG
+                            }
+                        ).ToList();
+
+            int i = 0;
+            var ListMatHang = (
+                                from mh in db.MATHANGs.ToList()
+                                select new
+                                {
+                                    ID = mh.MAMH,
+                                    TenMatHang = mh.TENMH,
+                                    SoLuong = ListCTB.Where(p => p.MaMH == mh.MAMH).Sum(p => p.SoLuong),
+                                    TongTien = ListCTB.Where(p => p.MaMH == mh.MAMH).Sum(p => p.SoLuong) * mh.DONGIA
+                                    
+                                }
+                              )
+                              .Where(p => p.SoLuong > 0)
+                              .OrderByDescending(p=>p.TongTien)
+                              .Select(p => new
+                              {
+                                  STT = ++i,
+                                  ID = p.ID,
+                                  MatHang = p.TenMatHang,
+                                  SoLuong = p.SoLuong,
+                                  TongTien = ((int)p.TongTien).ToString("N0"),
+                                  gt = p.TongTien
+                              })
+                              .ToList();
+
+            txtLoiNhuan.Text = "Tổng lợi nhuận : " + ((int)ListMatHang.Sum(p => p.gt).Value).ToString("N0");
+
+            dgrThongKe.DataSource = ListMatHang;
+
+            
+
+
+            chartThongKe.DataSource = ListMatHang;
+            chartThongKe.Series[0].XValueMember = "MatHang";
+            chartThongKe.Series[0].XValueType = ChartValueType.String;
+            chartThongKe.Series[0].YValueMembers = "TongTien";
+            chartThongKe.Series[0].YValueType = ChartValueType.Int32;
         }
         #endregion
 
